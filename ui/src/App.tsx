ebd5164ed435fa9fb380f5e975fe0990adf9f9ec
Adrian Lyjak <adrianlyjak@gmail.com>
2025-09-16 00:22:17 -0400

    Simplify basic ui starter (#2)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,14 +1,21 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Confetti from "./pages/Confetti";
+import { ApiProvider, ApiClients, createWorkflowClient } from "@llamaindex/ui";
+
+const deploymentName =
+  import.meta.env.VITE_LLAMA_DEPLOY_DEPLOYMENT_NAME || "default";
+const api: ApiClients = {
+  workflowsClient: createWorkflowClient({
+    baseUrl: `/deployments/${deploymentName}`,
+  }),
+};
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/confetti" element={<Confetti />} />
-    </Routes>
+    <ApiProvider clients={api}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </ApiProvider>
   );
 }
-
-
